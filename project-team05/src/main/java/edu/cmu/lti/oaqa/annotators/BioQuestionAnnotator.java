b9634cb13a59945a4030e2b470c64a9517fb11d2
niloygupta <niloygupta@gmail.com>
2014-11-09 18:10:58 -0500

    Using the recommended type systems

--- project-team05/src/main/java/edu/cmu/lti/oaqa/annotators/BioQuestionAnnotator.java
+++ project-team05/src/main/java/edu/cmu/lti/oaqa/annotators/BioQuestionAnnotator.java
@@ -1,13 +1,10 @@
 package edu.cmu.lti.oaqa.annotators;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.FSIterator;
-import org.apache.uima.fit.util.FSCollectionFactory;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 
@@ -20,9 +17,9 @@ import edu.cmu.lti.oaqa.bio.bioasq.services.PubMedSearchServiceResponse.Document
 import edu.cmu.lti.oaqa.bio.bioasq.services.PubMedSearchServiceResponse.MeshAnnotation;
 import edu.cmu.lti.oaqa.type.input.Question;
 import edu.cmu.lti.oaqa.type.kb.Concept;
-import edu.cmu.lti.oaqa.type.kb.ConceptMention;
-import edu.cmu.lti.oaqa.type.kb.DocumentP;
-import edu.cmu.lti.oaqa.type.kb.RDFTriples;
+import edu.cmu.lti.oaqa.type.kb.Triple;
+import edu.cmu.lti.oaqa.type.retrieval.ConceptSearchResult;
+import edu.cmu.lti.oaqa.type.retrieval.TripleSearchResult;
 
 
 
@@ -44,37 +41,30 @@ public class BioQuestionAnnotator extends JCasAnnotator_ImplBase {
       //System.out.println(question.getQuestionType() + " "+question.getText());
       String questionText = StanfordLemmatizer.stemText(question.getText());
       System.out.println(question.getQuestionType() + " "+questionText);
-      HashMap<String,List<String>> conceptMap = new HashMap<String,List<String>>();
-      ConceptMention conceptMention; 
+      ConceptSearchResult conceptSearchResult;
       List<OntologyServiceResponse.Result> resultList = GoPubMedServiceSingleton.getService().getConcepts(questionText);
+      int rank = 0;
       for(OntologyServiceResponse.Result resultResponse:resultList)
       {
         for (OntologyServiceResponse.Finding finding : resultResponse.getFindings())
         {
-          if(!conceptMap.containsKey(finding.getConcept().getLabel()))
-            conceptMap.put(finding.getConcept().getLabel(), new ArrayList<String>());
-          conceptMap.get(finding.getConcept().getLabel()).add(finding.getConcept().getUri());
-
+          conceptSearchResult = new ConceptSearchResult(jcas);
+          Concept concept = new Concept(jcas);
+          concept.setName(finding.getConcept().getLabel());
+          conceptSearchResult.setConcept(concept);
+          conceptSearchResult.setUri(finding.getConcept().getUri());
+          conceptSearchResult.setRank(rank++);
+          conceptSearchResult.addToIndexes();
         }
       }
-      for(String conceptLabel:conceptMap.keySet())
-      {
-        conceptMention =  new ConceptMention(jcas);
-        Concept concept = new Concept(jcas);
-        concept.setName(conceptLabel);
-        concept.setUris(FSCollectionFactory.createStringList(jcas, conceptMap.get(conceptLabel)));
-        conceptMention.setConcept(concept);
-        //conceptMention.addToIndexes();
-        jcas.addFsToIndexes(conceptMention);
-      }
-     // SearchResult searchResult = new SearchResult(jcas);
-      DocumentP doc; 
+      edu.cmu.lti.oaqa.type.retrieval.Document doc; 
       PubMedSearchServiceResponse.Result documents = GoPubMedServiceSingleton.getService().getDocuments(questionText);
       if(documents!=null && documents.getDocuments()!=null && !documents.getDocuments().isEmpty())
       {
+        rank = 0;
         for(Document document:documents.getDocuments())
         {
-          doc = new DocumentP(jcas);
+          doc = new edu.cmu.lti.oaqa.type.retrieval.Document(jcas);
           List<MeshAnnotation> meshAnnotations = document.getMeshAnnotations();
           System.out.println(document.getTitle());
           for(MeshAnnotation annotation:meshAnnotations)
@@ -82,8 +72,10 @@ public class BioQuestionAnnotator extends JCasAnnotator_ImplBase {
             System.out.println(annotation.getTermLabel());
             System.out.println(annotation.getUri());
           }
-          doc.setPmid(document.getPmid());
-          jcas.addFsToIndexes(doc);
+          doc.setDocId(document.getPmid());
+          doc.setUri(document.getPmid());
+          doc.setRank(rank++);
+          doc.addToIndexes();
         }
       }
       if("YES_NO".equals(question.getQuestionType()))
@@ -91,17 +83,22 @@ public class BioQuestionAnnotator extends JCasAnnotator_ImplBase {
         LinkedLifeDataServiceResponse.Result linkedLifeDataResult = GoPubMedServiceSingleton.getService().getTriples(questionText);
         if(linkedLifeDataResult != null)
         {
-          RDFTriples triple;
+          TripleSearchResult tripleSearchResult;
+          rank = 0;
           for (LinkedLifeDataServiceResponse.Entity entity : linkedLifeDataResult.getEntities()) {
             for (LinkedLifeDataServiceResponse.Relation relation : entity.getRelations()) {
-              triple = new RDFTriples(jcas);
+              tripleSearchResult = new TripleSearchResult(jcas);
+              tripleSearchResult.setRank(rank++);
+              Triple triple = new Triple(jcas);              
               triple.setObject(relation.getObj());
               triple.setSubject(relation.getSubj());
               triple.setPredicate(relation.getPred());
-              System.out.println("   - pred: " + relation.getPred());
+              tripleSearchResult.setTriple(triple);
+              tripleSearchResult.addToIndexes();
+              /*System.out.println("   - pred: " + relation.getPred());
               System.out.println("   - sub: " + relation.getSubj());
               System.out.println("   - obj: " + relation.getObj());
-              jcas.addFsToIndexes(triple);
+              jcas.addFsToIndexes(triple);*/
             }
           }
         }
