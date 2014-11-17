4a3d83fe92447267ebd7183acce2d1052bba18f3
Hakim <hakimsd@gmx.com>
2014-11-16 23:12:52 -0500

    Parse result from  http://metal.lti.cs.cmu.edu:30002/pmc/PMID Takes document as input Returns raw text as a string

---
+++ project-team05/src/main/java/util/FullDocumentSources.java
@@ -0,0 +1,71 @@
+package util;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+
+import edu.cmu.lti.oaqa.bio.bioasq.services.PubMedSearchServiceResponse.Document;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.stream.JsonReader;
+
+public class FullDocumentSources {
+
+  static String url = "http://metal.lti.cs.cmu.edu:30002/pmc/";
+
+
+  public static String getFullText(Document doc) throws IOException{
+
+    String rawText = new String();
+    
+    if (doc.isFulltextAvailable()){
+      String pmid = doc.getPmid();
+      String request = url + pmid;
+
+      URL serviceURL = new URL(request);
+      InputStream is = serviceURL.openStream();
+      InputStreamReader isr = new InputStreamReader(is);
+      BufferedReader br = new BufferedReader(isr);
+      String response = br.readLine();
+      
+      // The service sends one line response only
+      // parse it 
+      InputStream stream = IOUtils.toInputStream(response, "UTF-8");
+
+      JsonReader reader = new JsonReader(new InputStreamReader(stream, "UTF-8"));
+    
+      GsonBuilder builder = new GsonBuilder();
+      Object o = builder.create().fromJson(reader, Object.class);
+    
+      com.google.gson.internal.LinkedTreeMap<String, List<String>> o2 = (com.google.gson.internal.LinkedTreeMap) o; 
+
+      List<String> text = o2.get("sections");
+    
+      for (String s: text){
+        rawText += s;
+      }
+    }
+    
+    return rawText;
+    
+  }
+  
+}
+
+
