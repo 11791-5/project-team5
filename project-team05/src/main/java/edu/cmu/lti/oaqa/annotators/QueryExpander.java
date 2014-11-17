08bc7b5969e8672b06fa8e016457f1ee244b73a7
Maya Tydykov <mtydykov@andrew.cmu.edu>
2014-11-17 18:27:07 -0500

    don't pass stopwords in the synset list down the pipeline

--- project-team05/src/main/java/edu/cmu/lti/oaqa/annotators/QueryExpander.java
+++ project-team05/src/main/java/edu/cmu/lti/oaqa/annotators/QueryExpander.java
@@ -25,7 +25,6 @@ import edu.stanford.nlp.ling.CoreLabel;
 
 public class QueryExpander extends JCasAnnotator_ImplBase {
 
-
   @Override
   public void process(JCas jcas) throws AnalysisEngineProcessException {
     FSIterator<Annotation> questions = jcas.getAnnotationIndex(Question.type).iterator();
@@ -35,20 +34,23 @@ public class QueryExpander extends JCasAnnotator_ImplBase {
       expandedQuestion.setText(question.getText());
       expandedQuestion.setQuestionType(question.getQuestionType());
       expandedQuestion.setId(question.getId());
-      // System.out.println(question.getQuestionType() + " "+question.getText());
+
       String questionText = StanfordLemmatizer.stemText(question.getText());
       edu.stanford.nlp.pipeline.Annotation ann = new edu.stanford.nlp.pipeline.Annotation(
               questionText);
       StanfordAnnotatorSingleton.getInstance().getPipeline().annotate(ann);
       ArrayList<SynSet> synSets = new ArrayList<SynSet>();
+
+      // add EACH term from original query to synSets
       for (CoreLabel term : ann.get(TokensAnnotation.class)) {
-        SynSet sSet = new SynSet(jcas);
-        sSet.setOriginalToken(term.originalText());
-        HashMap<String, Double> synToConfidence = new HashMap<String, Double>();
+
         if (!StopWordSingleton.getInstance().isStopWord(term.originalText())) {
+          SynSet sSet = new SynSet(jcas);
+          sSet.setOriginalToken(term.originalText());
+          HashMap<String, Double> synToConfidence = new HashMap<String, Double>();
           try {
-            HashSet<TermRelationship> rels = new HashSet<TermRelationship>(
-                    UmlsSingleton.getInstance().getUmlsService().getTermSynonyms(term.originalText()));
+            HashSet<TermRelationship> rels = new HashSet<TermRelationship>(UmlsSingleton
+                    .getInstance().getUmlsService().getTermSynonyms(term.originalText()));
             for (TermRelationship rel : rels) {
               if (!synToConfidence.containsKey(rel.getToTerm())) {
                 synToConfidence.put(rel.getToTerm(), rel.getConfidence());
@@ -61,17 +63,17 @@ public class QueryExpander extends JCasAnnotator_ImplBase {
           } catch (Exception e) {
             e.printStackTrace();
           }
+          HashSet<Synonym> synList = new HashSet<Synonym>();
+          for (String synonym : synToConfidence.keySet()) {
+            Synonym syn = new Synonym(jcas);
+            syn.setSynonym(synonym);
+            syn.setConfidence(synToConfidence.get(synonym));
+            syn.setSource("UMLS");
+            synList.add(syn);
+          }
+          sSet.setSynonyms(Utils.fromCollectionToFSList(jcas, synList));
+          synSets.add(sSet);
         }
-        HashSet<Synonym> synList = new HashSet<Synonym>();
-        for (String synonym : synToConfidence.keySet()) {
-          Synonym syn = new Synonym(jcas);
-          syn.setSynonym(synonym);
-          syn.setConfidence(synToConfidence.get(synonym));
-          syn.setSource("UMLS");
-          synList.add(syn);
-        }
-        sSet.setSynonyms(Utils.fromCollectionToFSList(jcas, synList));
-        synSets.add(sSet);
       }
       expandedQuestion.setSynSets(Utils.fromCollectionToFSList(jcas, synSets));
       expandedQuestion.addToIndexes();
