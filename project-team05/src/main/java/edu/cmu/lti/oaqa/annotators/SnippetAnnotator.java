fee26a6e24079577d1bdb7801db1398daf05cb48
niloygupta <niloygupta@gmail.com>
2014-11-23 19:39:33 -0500

    Restoring Snippet Annotator. Had deleted it by mistake.

---
+++ project-team05/src/main/java/edu/cmu/lti/oaqa/annotators/SnippetAnnotator.java
@@ -0,0 +1,225 @@
+package edu.cmu.lti.oaqa.annotators;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import json.gson.Snippet;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.TOP;
+import org.apache.uima.jcas.tcas.Annotation;
+
+import util.FullDocumentSources;
+import util.SimilarityMeasures;
+import util.Utils;
+import edu.cmu.lti.oaqa.type.input.ExpandedQuestion;
+import edu.cmu.lti.oaqa.type.retrieval.Passage;
+import edu.cmu.lti.oaqa.type.retrieval.SnippetSearchResult;
+import edu.cmu.lti.oaqa.type.retrieval.SynSet;
+import edu.cmu.lti.oaqa.type.retrieval.Synonym;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+
+public class SnippetAnnotator extends JCasAnnotator_ImplBase {
+
+  private static final int  SnippetRetrievedNum = 1000;
+
+  public static final String PUBMED_URL = "http://www.ncbi.nlm.nih.gov/pubmed/";
+
+  private static final int MinConceptMatch = 2;
+
+  public class ScoreComparator implements Comparator<Snippet> {
+    public int compare(Snippet o1, Snippet o2) {
+
+      if (o2.score > o1.score)
+        return 1;
+      else if (o2.score<o1.score)
+        return -1;
+      else
+        return 0;
+    }
+  }
+
+  FileWriter snippetWriter = null;
+
+  File snippet = new File("snippetResults.txt");
+
+  public void initialize(UimaContext u) {
+    try {
+      snippetWriter = new FileWriter(snippet);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void collectionProcessComplete() {
+    try {
+      snippetWriter.flush();
+      snippetWriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void process(JCas jcas) throws AnalysisEngineProcessException {
+    int rank;
+
+    FSIterator<Annotation> questions = jcas.getAnnotationIndex(ExpandedQuestion.type).iterator();
+
+    while (questions.hasNext()) {
+      ExpandedQuestion question = (ExpandedQuestion) questions.next();
+
+      System.out.println(question.getSynSets());
+      ArrayList<SynSet> as = Utils.fromFSListToCollection(question.getSynSets(), SynSet.class);
+
+      edu.cmu.lti.oaqa.type.retrieval.SynSet synset;
+
+      String query = "";
+      ArrayList<ArrayList<String>> synonymListByGroup = new ArrayList<ArrayList<String>>();
+      ArrayList<String> synonymList = new ArrayList<String>();
+      for (SynSet syns : as) {
+        ArrayList<String> tempAL = new ArrayList<String>();
+        ArrayList<Synonym> synonyms = Utils.fromFSListToCollection(syns.getSynonyms(),
+                Synonym.class);
+        for (Synonym synonym : synonyms) {
+          synonymList.add(synonym.getSynonym());
+          tempAL.add(synonym.getSynonym());
+
+        }
+        synonymListByGroup.add(tempAL);
+        synonymList.add(syns.getOriginalToken());
+
+      }
+
+      List<edu.cmu.lti.oaqa.type.retrieval.Document> documentItems = getDocumentItems(jcas);
+
+      edu.cmu.lti.oaqa.type.retrieval.Document doc;
+
+      if (documentItems != null && !documentItems.isEmpty()) {
+        rank = 0;
+        ArrayList<Snippet> snippetList = new ArrayList<Snippet>();
+        SnippetSearchResult snippetSearchResult = new SnippetSearchResult(jcas);
+        for (edu.cmu.lti.oaqa.type.retrieval.Document document : documentItems) {
+          // System.out.println(document.getPmid());
+          doc = new edu.cmu.lti.oaqa.type.retrieval.Document(jcas);
+
+          List<String> text = null;
+
+          try {
+            text = FullDocumentSources.getFullText(document);
+            if (text == null)
+              continue;
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+          ArrayList<Snippet> snippets = new ArrayList<Snippet>();
+
+          // concept matching?
+
+          for (int i = 0; i < text.size(); i++) {
+            int offsetPtr = 0;
+            String docText = text.get(i);
+            Reader reader = new StringReader(docText);
+            DocumentPreprocessor dp = new DocumentPreprocessor(reader);
+
+            String nowSection = "section.0";
+            List<String> sentenceTokens;
+            for (List<HasWord> sentence : dp) {
+              sentenceTokens = new ArrayList<String>();
+              StringBuffer wholeSentence = new StringBuffer();
+              for (HasWord word : sentence) {
+                sentenceTokens.add(word.word());
+                wholeSentence.append(word.word() + " ");
+              }
+
+              int conceptMatch = 0;
+              String wholeSentenceStr = wholeSentence.toString();
+              
+              /*
+              for (ArrayList<String> synonymsGroup : synonymListByGroup) {
+                for (String synonymTempStr : synonymsGroup) {
+                  if (wholeSentenceStr.contains(synonymTempStr))
+                  {
+                    conceptMatch++;
+                    break;
+                  }
+                }
+              }
+              System.out.println( conceptMatch +" "+ wholeSentenceStr);
+              if (conceptMatch<MinConceptMatch)
+                 continue;
+              */
+              SimilarityMeasures sm = new SimilarityMeasures();
+              double score = sm.getSimilarity(sentenceTokens, synonymList);
+
+              Snippet s = new Snippet(score, PUBMED_URL + document.getDocId(),
+                      wholeSentence.toString(), offsetPtr, offsetPtr + sentence.size(), nowSection,
+                      nowSection);
+
+              snippetList.add(s);
+              offsetPtr = offsetPtr + sentence.size();
+
+            }
+          }
+        }
+        int rankThreshold = 1;
+        Collections.sort(snippetList, new ScoreComparator());
+
+        System.out.println(snippetList.size());
+        for (Snippet snippet : snippetList) {
+          try {
+            snippetWriter.write("Q:" + question.getText() + " Document:" + snippet.getDocument()
+                    + " offsetBegin: " + snippet.getOffsetInBeginSection() + " offsetEnd: "
+                    + snippet.getOffsetInEndSection() + " A: " + snippet.getText() + "\n");
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+
+          System.out.println(snippet.score + " " + snippet.getText());
+
+          snippetSearchResult.setSnippets(createPassage(snippet, jcas));
+          rankThreshold++;
+
+          snippetSearchResult.addToIndexes();
+
+          if (rankThreshold > SnippetRetrievedNum)
+            break;
+        }
+      }
+    }
+  }
+
+  private Passage createPassage(Snippet snippet, JCas jcas) {
+    Passage passage = new Passage(jcas);
+    passage.setDocId(snippet.getDocument());
+    passage.setBeginSection(snippet.getBeginSection());
+    passage.setEndSection(snippet.getEndSection());
+    passage.setOffsetInBeginSection(snippet.getOffsetInBeginSection());
+    passage.setOffsetInEndSection(snippet.getOffsetInEndSection());
+    passage.setText(snippet.getText());
+    return passage;
+  }
+
+  private List<edu.cmu.lti.oaqa.type.retrieval.Document> getDocumentItems(JCas jcas) {
+    FSIterator<TOP> documentsIter = jcas.getJFSIndexRepository().getAllIndexedFS(
+            edu.cmu.lti.oaqa.type.retrieval.Document.type);
+    List<edu.cmu.lti.oaqa.type.retrieval.Document> documentItems = new ArrayList<edu.cmu.lti.oaqa.type.retrieval.Document>();
+    while (documentsIter.hasNext()) {
+      edu.cmu.lti.oaqa.type.retrieval.Document document = (edu.cmu.lti.oaqa.type.retrieval.Document) documentsIter
+              .next();
+      documentItems.add(document);
+    }
+    return documentItems;
+  }
+}
