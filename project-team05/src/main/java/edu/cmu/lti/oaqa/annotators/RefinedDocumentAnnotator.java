7863dd0473d8cff2104ba6e98f882a0230802e88
Maya Tydykov <mtydykov@andrew.cmu.edu>
2014-11-26 11:24:25 -0500

    cleaned up duplication

---
+++ project-team05/src/main/java/edu/cmu/lti/oaqa/annotators/RefinedDocumentAnnotator.java
@@ -0,0 +1,18 @@
+package edu.cmu.lti.oaqa.annotators;
+
+import java.util.List;
+
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+
+import util.GoPubMedServiceSingleton;
+import edu.cmu.lti.oaqa.bio.bioasq.services.PubMedSearchServiceResponse;
+import edu.cmu.lti.oaqa.bio.bioasq.services.PubMedSearchServiceResponse.Document;
+import edu.cmu.lti.oaqa.bio.bioasq.services.PubMedSearchServiceResponse.MeshAnnotation;
+import edu.cmu.lti.oaqa.type.input.ExpandedQuestion;
+
+public class RefinedDocumentAnnotator extends DocumentAnnotator
+{}
