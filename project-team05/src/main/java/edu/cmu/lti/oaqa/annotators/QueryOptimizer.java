d78696bbb64f39e19960e127eb28e5425aedf3c9
Hakim <hakimsd@gmx.com>
2014-11-27 11:21:42 -0500

    added ontologies and comments

--- project-team05/src/main/java/edu/cmu/lti/oaqa/annotators/QueryOptimizer.java
+++ project-team05/src/main/java/edu/cmu/lti/oaqa/annotators/QueryOptimizer.java
@@ -1,25 +1,27 @@
 package edu.cmu.lti.oaqa.annotators;
+
 import java.util.ArrayList;
 import util.Utils;
 import edu.cmu.lti.oaqa.type.input.ExpandedQuestion;
 import edu.cmu.lti.oaqa.type.retrieval.SynSet;
 import edu.cmu.lti.oaqa.type.retrieval.Synonym;
-public class QueryOptimizer
-{
-public static String optimizeQuery(ExpandedQuestion question)
-{
-ArrayList<SynSet> as = Utils.fromFSListToCollection(question.getSynSets(), SynSet.class);
-edu.cmu.lti.oaqa.type.retrieval.SynSet synset;
-ArrayList<String> synonymList = new ArrayList<String>();
-String query = "(";
-for (SynSet syns : as)
-{
-ArrayList<Synonym> synonyms = Utils.fromFSListToCollection(syns.getSynonyms(),Synonym.class);
-for (Synonym synonym : synonyms)
-query+= synonym.getSynonym() + " OR ";
-query += syns.getOriginalToken() + " )AND( ";
-}
-query = query.substring(0, query.length()-5);
-return query;
-}
+
+public class QueryOptimizer {
+  public static String optimizeQuery(ExpandedQuestion question) {
+    ArrayList<SynSet> as = Utils.fromFSListToCollection(question.getSynSets(), SynSet.class);
+    edu.cmu.lti.oaqa.type.retrieval.SynSet synset;
+    ArrayList<String> synonymList = new ArrayList<String>();
+    // Form a query as (term1 OR its synonyms)AND(term2 OR its synonyms)etc
+    String query = "(";
+    for (SynSet syns : as) {
+      ArrayList<Synonym> synonyms = Utils.fromFSListToCollection(syns.getSynonyms(), Synonym.class);
+      for (Synonym synonym : synonyms)
+        // OR each term with its synonyms
+        query += synonym.getSynonym() + " OR ";
+      // AND each group of synonyms
+      query += syns.getOriginalToken() + " )AND( ";
+    }
+    query = query.substring(0, query.length() - 5);
+    return query;
+  }
 }
